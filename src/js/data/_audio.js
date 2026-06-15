6b68632a63b00338f7e6f5926daf5859df72eadb
Asya Lebedeva <star@asyaspace.com>
2026-06-15 23:55:18 +0700

    build: add an array with objs, script data

---
+++ src/js/data/_audio.js
@@ -0,0 +1,31 @@
+export const tracks = [
+    {
+        title: '3LAU, Bright Lights - How You Love Me',
+        src: '/../src/assets/audio/how_you_love_me_track.ogg'
+    },
+
+    {
+        title: 'Bright Lights, Kaleena Zanders, Kandy - War For Love',
+        src: '/../src/assets/audio/war_for_love_track.ogg'
+    },
+    
+    {
+        title: 'Pink Is Punk, Benny Benassi, Bright Lights - Ghost',
+        src: '/../src/assets/audio/ghost_track.ogg'
+    },
+
+    {
+        title: 'Hardwell, Dyro, Bright Lights - Never Say Goodbye',
+        src: '/../src/assets/audio/never_say_goodbye_track.ogg'
+    },
+    
+    {
+        title: 'Zeds Dead, Dirtyphonics, Bright Lights - Where Are You Now',
+        src: '/../src/assets/audio/where_are_you_now_track.ogg'
+    },
+
+    {
+        title: 'Zedd, Bright Lights - Follow You Down',
+        src: '/../src/assets/audio/follow_you_down_track.ogg'
+    },
+]
