ba9d2e201908d887b2ec9751dba805c0c1696733
Asya Lebedeva <star@asyaspace.com>
2026-06-17 04:42:10 +0700

    fix: change audio paths, thumb stylesheet for deploy

--- src/js/data/_data.js
+++ src/js/data/_data.js
@@ -1,31 +1,31 @@
 export const tracks = [
     {
         title: '3LAU, Bright Lights - How You Love Me',
-        src: '/../src/assets/audio/how_you_love_me_track.ogg'
+        src: '/audio/how-you-love-me.ogg'
     },
 
     {
         title: 'Bright Lights, Kaleena Zanders, Kandy - War For Love',
-        src: '/../src/assets/audio/war_for_love_track.ogg'
+        src: '/audio/war-for-love.ogg'
     },
     
     {
         title: 'Pink Is Punk, Benny Benassi, Bright Lights - Ghost',
-        src: '/../src/assets/audio/ghost_track.ogg'
+        src: '/audio/ghost.ogg'
     },
 
     {
         title: 'Hardwell, Dyro, Bright Lights - Never Say Goodbye',
-        src: '/../src/assets/audio/never_say_goodbye_track.ogg'
+        src: '/audio/never-say-goodbye.ogg'
     },
     
     {
         title: 'Zeds Dead, Dirtyphonics, Bright Lights - Where Are You Now',
-        src: '/../src/assets/audio/where_are_you_now_track.ogg'
+        src: '/audio/where-are-you-now.ogg'
     },
 
     {
         title: 'Zedd, Bright Lights - Follow You Down',
-        src: '/../src/assets/audio/follow_you_down_track.ogg'
+        src: '/audio/follow-you-down.ogg'
     },
 ]
