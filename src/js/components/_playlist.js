8434dc8e8e7ba996614ba22b50142fe25a6df8f8
Asya Lebedeva <star@asyaspace.com>
2026-06-17 01:26:49 +0700

    refactor: separate functions between boths players, add dynamic playlist

--- src/js/components/_playlist.js
+++ src/js/components/_playlist.js
@@ -1,39 +1,67 @@
-import { tracks } from './../data/_audio.js';
+import { tracks } from '../data/_data.js';
+import { showTime } from './_player.js';
 import { showCommonTime } from './_player.js';
+import { playPause } from './_player.js';
+import { changeBtn } from './_player.js';
+import { userChangeProgress } from './_player.js';
+import { updPlayerAndTiming } from './_player.js';
 
 const list = document.querySelector('.tracks__list'),
-      runBtn = document.querySelectorAll('.controller__play'),
-      progress = document.querySelectorAll('.controller__timeline'),
-      song = document.querySelector('.init__audio');
+      runBtn = document.querySelectorAll('.controller__play')[1],
+      progress = document.querySelectorAll('.controller__timeline')[1],
+      audio = document.querySelector('.init__audio'); // <= who is playing audio? <audio>
 
-/* generation playlist */
-tracks.forEach(track => {
-    list.insertAdjacentHTML(
+/*  1) generation playlist without audio for correct generation'; 
+    just one empty tag <audio> in HTML without attribute "src"
+    list is just titles (6 counts), no audio src */
+tracks.forEach((track) => {
+    list.insertAdjacentHTML(       
         'beforeend',
         `<li class="tracks__item">
             <p class="tracks__title">${track.title}</p>
         </li>`
-    )
+    );
 })
 
+/* 2) 0 = the 1st track from list;
+    load 1st track */
 let currentIndex = 0;
 function loadTrack(index) {
-    song.src = tracks[index].src;
-    song.load();
-
-    console.log(tracks[index].src);
+    audio.src = tracks[index].src;
+    audio.load();
 }
 loadTrack(currentIndex);
 
-function playPause(index) {
-    if(song.paused) {
-        song.play();
-        runBtn[1].className = 'controller__play';
-    } else {
-        song.pause();
-        runBtn[1].className = 'controller__pause';
-    }
-}
-runBtn[1].addEventListener('click', playPause);
-//let currentIndex = 0;
+
+runBtn.addEventListener('click', () => playPause(audio, runBtn));
+
+// 3) list from generated track's list, look at tracks.forEach();
+const items = document.querySelectorAll('.tracks__item'),
+      titles = document.querySelectorAll('.tracks__title');
+items.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        currentIndex = index;
+        // load track with index n
+        loadTrack(currentIndex);
+        audio.play();
+        // coloring playing track
+        titles.forEach((title, i) => {
+            if (i === currentIndex) {
+                title.classList.add('active');
+            } else {
+                title.classList.remove('active');
+            }
+        });
+    });
+})
+
+const currentTime = document.querySelectorAll('.controller__current')[1],
+      commonTime = document.querySelectorAll('.controller__duration')[1];
+audio.addEventListener('loadedmetadata', () =>  showCommonTime(audio, commonTime));
+audio.addEventListener('timeupdate', () => updPlayerAndTiming(audio, progress, currentTime));
+changeBtn(audio, runBtn);
+progress.addEventListener('input', () => userChangeProgress(audio, progress));
+
+
+
 
